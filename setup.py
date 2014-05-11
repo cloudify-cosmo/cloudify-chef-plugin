3499bd10c102dc9324916c40b9cf2e79f6a0e43b
Dan Kilman <dankilman@gmail.com>
2014-05-11 12:34:28 +0300

    CFY-637 update version

--- setup.py
+++ setup.py
@@ -18,15 +18,15 @@ __author__ = 'dank'
 
 import setuptools
 
-PLUGIN_COMMONS_VERSION = '3.0b1'
-PLUGIN_COMMONS_BRANCH = '3.0b1'
+PLUGIN_COMMONS_VERSION = '3.0'
+PLUGIN_COMMONS_BRANCH = '3.0'
 PLUGIN_COMMONS = 'https://github.com/cloudify-cosmo/cloudify-plugins-common' \
     '/tarball/{0}'.format(PLUGIN_COMMONS_BRANCH)
 
 setuptools.setup(
     zip_safe=False,
     name='cloudify-chef-plugin',
-    version='1.0b1',
+    version='1.0',
     author='ilya',
     author_email='ilya.sher@coding-knight.com',
     packages=['chef_plugin'],
