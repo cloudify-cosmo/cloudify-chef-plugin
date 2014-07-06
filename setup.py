73bc606f83e24633fc7732be546bd6c989f56360
Dan Kilman <dankilman@gmail.com>
2014-07-06 12:41:56 +0300

    update common version dependency

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
     license='LICENSE',
     description='Cloudify Chef plugin',
     install_requires=[
-        'cloudify-plugins-common==3.0',
+        'cloudify-plugins-common>=3.0',
         'requests',
     ],
     package_data={
