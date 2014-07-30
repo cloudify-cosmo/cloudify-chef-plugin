3d4ed38087a66c4e8307e599423cc489d1e391f0
opencm <limor@gigaspaces.com>
2014-07-30 12:18:22 +0300

    Bump version to 3.1m1 / 1.1m1

--- setup.py
+++ setup.py
@@ -22,14 +22,14 @@ import setuptools
 setuptools.setup(
     zip_safe=False,
     name='cloudify-chef-plugin',
-    version='1.1',
+    version='1.1a1',
     author='ilya',
     author_email='ilya.sher@coding-knight.com',
     packages=['chef_plugin'],
     license='LICENSE',
     description='Cloudify Chef plugin',
     install_requires=[
-        'cloudify-plugins-common==3.1',
+        'cloudify-plugins-common==3.1a1',
         'requests',
     ],
     package_data={
